5db92f0ba219e92c9d27396d3150dc8db8b27bf5
Kaiibott <69609355+KaiibottAI@users.noreply.github.com>
2025-05-30 15:36:38 -0500

    - Building macros for the FoundryRadio. Need to get Sockets working.

--- scripts/foundryradio.js
+++ scripts/foundryradio.js
@@ -1,11 +1,11 @@
-const FOUNDRY_RADIO_MODULE = 'foundry-radio'
+const foundryRadioModuleName = 'foundry-radio'
 
 class FoundryRadio extends Application {
 
     // Please help, I don't know how to get some default stuff to stick. I have it down in the ready hook since I can't figure it :D
     static get defaultOptions() {
         return foundry.utils.mergeObject(super.defaultOptions, {
-            id: FOUNDRY_RADIO_MODULE,
+            id: foundryRadioModuleName,
             title: 'Foundry Radio',
             resizable: false,
             width: 400,
@@ -13,10 +13,6 @@ class FoundryRadio extends Application {
             template: "modules/foundry-radio/templates/foundryradio.html",
             classes: ["foundry-radio"]
         });
-    }
-
-    getData() {
-
     }
 
     activeListeners(html) {
@@ -38,17 +34,41 @@ function toggleFoundryRadio() {
 
 // Most of these functions are self explanitory by their name
 function applyRadioSkin(theme) {
-    game.settings.set(FOUNDRY_RADIO_MODULE, 'radioSkin', theme);
+    game.settings.set(foundryRadioModuleName, 'radioSkin', theme);
     document.documentElement.setAttribute('data-radio-theme', theme);
 };
 function updateRadioFont(selectedFont) {
     document.documentElement?.style.setProperty("--radio-font-family", `"${selectedFont}"`);
 };
 
+function _getPlaylist() {
+    const [playlist] = game.playlists.playing
+    return playlist ?? null;
+};
+
+function _getCurrentSong() {
+    const playlist = _getPlaylist();
+    if (!playlist) return null;
+
+    const [currentSong] = playlist.sounds.filter(sound => sound.playing);
+    return currentSong ?? null;
+};
+
+async function _nextSong() {
+    const playlist = _getPlaylist();
+    if (!playlist) {
+        ui.notifications.warn("No playlist is currently playing.");
+        return;
+    };
+
+    playlist.playNext();
+
+};
+
 Hooks.once("init", () => {
 
     // Set up all the module settings
-    game.settings.register(FOUNDRY_RADIO_MODULE, 'radioSkin', {
+    game.settings.register(foundryRadioModuleName, 'radioSkin', {
         name: 'Foundry Radio Skin',
         hint: 'Collection of pre-made themes for the Foundry Radio to have a unique style.',
         scope: 'world',
@@ -56,9 +76,10 @@ Hooks.once("init", () => {
         type: String,
         choices: {
             "cyberpunk-red": "CyberpunkRED",
+            "cyberpunk-edgerunners": "Cyberpunk Edgerunner",
             "deep-blue": "Deep Blue",
             "fallout-nuclear": "Fallout Terminal Green",
-            "classic": "Classic"
+            "mgs-codec": "MGS Codec"
         },
         default: 'cyberpunk-red',
         onChange: (value) => {
@@ -66,7 +87,7 @@ Hooks.once("init", () => {
         },
         requiresReload: false
     });
-    game.settings.register(FOUNDRY_RADIO_MODULE, 'title', {
+    game.settings.register(foundryRadioModuleName, 'title', {
         name: 'Foundry Radio Title',
         hint: 'The title of the Foundry Radio window that will show to the players.',
         scope: 'world',
@@ -86,7 +107,7 @@ Hooks.once("init", () => {
 Hooks.once("ready", () => {
 
     // This has to load later since Foundry loads fonts at a different time that is past `init` :(
-    game.settings.register(FOUNDRY_RADIO_MODULE, 'RadioFont', {
+    game.settings.register(foundryRadioModuleName, 'RadioFont', {
         name: "Radio Font",
         hint: "Select the font for the Foundry Radio. Supports Custom Fonts if uploaded to Foundry Font Settings.",
         scope: "client",
@@ -96,14 +117,14 @@ Hooks.once("ready", () => {
         default: "Orbitron",
         onChange: (value) => {
             updateRadioFont(value);
-            game.settings.set(FOUNDRY_RADIO_MODULE, 'RadioFont', value);
+            game.settings.set(foundryRadioModuleName, 'RadioFont', value);
         },
         requiresReload: false
     });
 
 
     // Added these all down here since this is how I could get the settings to be 'retained' upon reloading. I still do not understand it.
-    applyRadioSkin(game.settings.get(FOUNDRY_RADIO_MODULE, 'radioSkin'));
-    updateRadioFont(game.settings.get(FOUNDRY_RADIO_MODULE, 'RadioFont'));
+    applyRadioSkin(game.settings.get(foundryRadioModuleName, 'radioSkin'));
+    updateRadioFont(game.settings.get(foundryRadioModuleName, 'RadioFont'));
 
 });
